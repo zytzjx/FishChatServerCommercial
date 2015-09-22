9b281b59d5f4ab52be100a1965eaba372c861dfc
oikomi <miaohonghit@gmail.com>
2015-09-22 17:30:15 -0400

    init

---
+++ info/msg_server_info.go
@@ -0,0 +1,28 @@
+package info
+
+// p2p
+const (
+	NO_CLIENT_INFO                = "No client info."
+	THE_ID_IS_ALREADY_YOUR_FRIEND = "The id is already your friend."
+	YOU_HAVE_NO_THIS_FRIEND       = "You have no this friend."
+	YOU_HAVE_NO_FRIENDS           = "You have no friends."
+	THIS_ID_IS_NOT_EXISTS         = "This id is not exists."
+	YOU_HAVE_TO_RE_LOGIN          = "You have to re-login."
+	NO_INITIATE_THIS_REQUEST      = "Users did not initiate this request."
+)
+
+//Topic
+const (
+	ERROR                 = "error."
+	CREATE_TOPIC_FAILURE  = "Create topic failure."
+	TOPIC_ALREADY_EXISTS  = "Topic already exists."
+	TOPIC_DOES_NOT_EXISTS = "Topic does not exist."
+	YOU_HAVE_NO_TOPICS    = "You have no topics."
+
+	JOIN_TOPIC_FAILURE = "Join topic failure."
+
+	YOU_WERE_NOT_IN_TOPIC = "You were not in this topic."
+	LEAVE_TOPIC_FAILURE   = "Leave topic failure."
+
+	NO_CLIENTS_IN_TOPIC = "No clients in topic."
+)
