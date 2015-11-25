aa0c654c630712607e788c3b30fabee4b7c48b02
oikomi <miaohonghit@gmail.com>
2015-11-25 16:22:07 -0500

    init

---
+++ gateway/proto_proc.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"goProject/common"
+	"goProject/libnet"
+	"goProject/log"
+	"goProject/protocol"
+)
+
+func init() {
+	flag.Set("alsologtostderr", "true")
+	flag.Set("log_dir", "false")
+}
+
+type ProtoProc struct {
+	gateway *Gateway
+}
+
+func NewProtoProc(gateway *Gateway) *ProtoProc {
+	return &ProtoProc{
+		gateway: gateway,
+	}
+}
+
+func (self *ProtoProc) procReqMsgServer(cmd *protocol.CmdSimple, session *libnet.Session) error {
+	log.Info("procReqMsgServer")
+	var err error
+	msgServer := common.SelectServer(self.gateway.msgServerList)
+
+	resp := protocol.NewCmdResponse(protocol.SELECT_MSG_SERVER_FOR_CLIENT_CMD)
+	resp.AddArg(msgServer)
+	resp.Repo = cmd.GetReport()
+
+	log.Info("Resp | ", resp)
+
+	if session != nil {
+		if err = session.Send(resp); err != nil {
+			log.Error(err.Error())
+		}
+		session.Close()
+
+		log.Info("client ", session.Conn().RemoteAddr().String(), " | close")
+	}
+	return err
+}
---
+++ service_discovery/master.go
@@ -0,0 +1,94 @@
+package service_discovery
+
+import (
+	"encoding/json"
+	"github.com/coreos/etcd/Godeps/_workspace/src/golang.org/x/net/context"
+	"github.com/coreos/etcd/client"
+	"goProject/common"
+	"goProject/log"
+	"time"
+)
+
+type Master struct {
+	Members map[string]*Member
+	KeysAPI client.KeysAPI
+}
+
+// Member is a client machine
+type Member struct {
+	InGroup bool
+	IP      string
+	Name    string
+	CPU     int
+}
+
+func NewMaster(endpoints []string) *Master {
+	cfg := client.Config{
+		Endpoints:               endpoints,
+		Transport:               client.DefaultTransport,
+		HeaderTimeoutPerRequest: time.Second,
+	}
+
+	etcdClient, err := client.New(cfg)
+	if err != nil {
+		log.Fatal("Error: cannot connec to etcd:", err)
+	}
+
+	master := &Master{
+		Members: make(map[string]*Member),
+		KeysAPI: client.NewKeysAPI(etcdClient),
+	}
+	go master.WatchWorkers()
+	return master
+}
+
+func (m *Master) AddWorker(info *WorkerInfo) {
+	member := &Member{
+		InGroup: true,
+		IP:      info.IP,
+		Name:    info.Name,
+		CPU:     info.CPU,
+	}
+	m.Members[member.Name] = member
+}
+
+func (m *Master) UpdateWorker(info *WorkerInfo) {
+	member := m.Members[info.Name]
+	member.InGroup = true
+}
+
+func (m *Master) WatchWorkers() {
+	api := m.KeysAPI
+	watcher := api.Watcher("workers/", &client.WatcherOptions{
+		Recursive: true,
+	})
+	for {
+		res, err := watcher.Next(context.Background())
+		if err != nil {
+			log.Error("Error watch workers:", err.Error())
+			break
+		}
+
+		if res.Action == "expire" {
+			key := common.Substr(res.Node.Key, len("/workers/"), len(res.Node.Key)-len("/workers/"))
+			member, ok := m.Members[key]
+			if ok {
+				member.InGroup = false
+			}
+		} else if res.Action == "set" || res.Action == "update" {
+			info := &WorkerInfo{}
+			err := json.Unmarshal([]byte(res.Node.Value), info)
+			if err != nil {
+				log.Error(err.Error())
+			}
+			if _, ok := m.Members[info.Name]; ok {
+				m.UpdateWorker(info)
+			} else {
+				m.AddWorker(info)
+			}
+		} else if res.Action == "delete" {
+			delete(m.Members, res.Node.Key)
+		}
+	}
+
+}
